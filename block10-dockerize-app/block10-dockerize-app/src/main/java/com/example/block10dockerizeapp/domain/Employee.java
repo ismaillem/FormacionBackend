7e9d1c602b058bcda8a1524d2223bb52845c9720
Ismail Lemrhari <ismail.lemrhari@bosonit.com>
2023-10-30 13:29:31 +0100

    bloque 10

---
+++ block10-dockerize-app/block10-dockerize-app/src/main/java/com/example/block10dockerizeapp/domain/Employee.java
@@ -0,0 +1,11 @@
+package com.example.block10dockerizeapp.domain;
+
+import lombok.Data;
+
+@Data
+public class Employee {
+    String employeeId;
+    String employeeName;
+    String employeeEmail;
+    String employeeAddress;
+}
